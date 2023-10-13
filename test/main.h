b6324d99b44373cea81071a6613037c2644bb092
ScottShadow <rudaseswascottmc@gmail.com>
2023-10-13 14:27:16 +0000

    handled %% added function for dummy printnum

---
+++ test/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*common library*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <strings.h>
+#include <stdarg.h>
+
+/*function prototypes*/
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif /*MAIN_H*/
