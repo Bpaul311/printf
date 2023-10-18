2311c805b2faf65bb9d6b08341f1a5bd41cfd315
Bpaul311 <bizimapaul777@gmail.com>
2023-10-18 06:54:42 -0700

    corrected the betty case and added not ptr condition

--- print_pointer.c
+++ print_pointer.c
@@ -10,10 +10,10 @@ void print_pointer(void *ptr)
 	uintptr_t p = (uintptr_t)ptr;
 	int buff = 0;
 
-	if (s == NULL)
+	if (s == NULL || !ptr)
 		return;
 	s[0] = '0';
-        s[1] = 'x';
+	s[1] = 'x';
 	buff += 2;
 	converter(p, 16, s, &buff, 0);
 	s[buff] = '\0';
